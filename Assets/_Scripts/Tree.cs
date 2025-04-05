27f718b5e10f7a50a0c7da2ab4605a252ba16f87
Bauke Hoekstra <bauke@meteormug.com>
2025-04-05 17:21:17 +0200

    Tree

--- Assets/_Scripts/Tree.cs
+++ Assets/_Scripts/Tree.cs
@@ -1,21 +1,39 @@
 using Sirenix.OdinInspector;
+using System.Collections;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class Tree : MonoBehaviour, IInteractable
 {
     [SerializeField] Rigidbody rb;
+    [SerializeField] FixedJoint[]joints;
     [SerializeField] Vector3 force;
     [SerializeField] int health = 1;
+    [SerializeField] GameObject leafs;
 
     [Button]
     public void OnInteract()
     {
         health--;
-        if(health <= 0)
+        if (health <= 0)
         {
+            leafs.SetActive(false);
+
             rb.isKinematic = false;
-            rb.AddForce(force);
-            //drop tree
+            rb.AddTorque(force);
+
+            StartCoroutine(BreakJoints(2));
+        }
+    }
+
+    IEnumerator BreakJoints(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        foreach (var joint in joints)
+        {
+            Destroy(joint);
         }
     }
 }
