e0b54630ce459ebed70c61459433381a75797b1f
Bauke Hoekstra <bauke@meteormug.com>
2025-04-05 16:31:36 +0200

    Tree

--- Assets/_Scripts/Tree.cs
+++ Assets/_Scripts/Tree.cs
@@ -1,14 +1,18 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class Tree : MonoBehaviour, IInteractable
 {
+    [SerializeField] Rigidbody rb;
     int health;
 
+    [Button]
     public void OnInteract()
     {
         health--;
         if(health <= 0)
         {
+            rb.isKinematic = false;
             //drop tree
         }
     }
