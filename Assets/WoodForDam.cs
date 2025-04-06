916b9c94b755b71e2b772fb318b3564b43000cd4
Bauke Hoekstra <bauke@meteormug.com>
2025-04-06 15:20:56 +0200

    Fix

--- Assets/WoodForDam.cs
+++ Assets/WoodForDam.cs
@@ -5,35 +5,20 @@ public class WoodForDam : MonoBehaviour
     public WoodCollector wCollector;
 
     private bool playerPresent;
-    //public string playerTag;
- 
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            //Debug.Log("yo");
-            if(wCollector == null)
+            if (wCollector == null)
                 wCollector = other.GetComponent<WoodCollector>();
 
             if (!playerPresent)
             {
                 playerPresent = true;
 
-                for (int i = wCollector.woods.Count - 1; i >= 0; i--)
-                {
-                    if (wCollector.woods[i] == null)
-                        continue;
-                    
-                    Destroy(wCollector.woods[i].gameObject);
-                    wCollector.woods.RemoveAt(i);
-                    UIManager.Singleton.UpdateItem(ItemTypes.Wood, -1);
-
-                    GameManager.Singleton.damCurrentHealth += 1;
-                    
-                    DamManager.Singleton.ChangeHealth();
-                }
-                
-                wCollector.woods.Clear();
+                GameManager.Singleton.damCurrentHealth += wCollector.RemoveAllWood(true);
+                DamManager.Singleton.ChangeHealth();
             }
         }
     }
